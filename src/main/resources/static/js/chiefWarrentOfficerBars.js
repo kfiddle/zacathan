c45c3f38546185824d3a607d090e54d9dba74d11
kfiddle <kenjfiddle@gmail.com>
2021-03-16 17:39:05 -0400

    scrolling bars

--- src/main/resources/static/js/chiefWarrentOfficerBars.js
+++ src/main/resources/static/js/chiefWarrentOfficerBars.js
@@ -1,5 +1,6 @@
 const bars = document.getElementById("bars");
-
+const sideBarButtonDiv = document.getElementById("sideBarButtonDiv");
+let barsArray = [];
 
 for (let j = 0; j < 3; j++) {
 
@@ -9,14 +10,45 @@ for (let j = 0; j < 3; j++) {
     bar.style.width = "22px";
     bar.style.height= "4px";
     bar.style.backgroundColor = "#1a1a1a"
-    bar.style.top = (j * 6) + "px";
+    bar.style.transition = "0.3s";
+    bar.style.top = (j * 6) + 2 + "px";
     bars.appendChild(bar);
+    barsArray.push(bar);
+}
+
+// sideBarButtonDiv.addEventListener('mouseover', ()=> {
+//     barsArray[0].style.opacity = "0.5";
+//     barsArray[0].style.top = "0px";
+// });
+
+sideBarButtonDiv.addEventListener('mouseover', ()=> {
+    scrollingBars("up");
+});
+
+sideBarButtonDiv.addEventListener('mouseout', ()=> {
+    scrollingBars("back");
+});
+
+// sideBarButtonDiv.addEventListener('mouseout', ()=> {
+//     barsArray[0].style.opacity = "1";
+//     barsArray[0].style.top = "2px";
+// })
+
+function resetBar(j, spinType) {
+    if (spinType === "up") {
+        barsArray[j].style.opacity = "0.5";
+        barsArray[j].style.top = (j * 6) - 2 + "px";
+    } else if (spinType === "back") {
+        barsArray[j].style.opacity = "1";
+        barsArray[j].style.top = (j * 6) + 2 + "px";
+    }
+}
+
+function scrollingBars(spinType) {
+    for (let j = 0; j < 3; j++) {
+        setTimeout(resetBar, j* 100, j, spinType);
+    }
+
 
 }
 
-//
-// display: block;
-// position: absolute;
-// width: 22px;
-// height: 4px;
-// background-color: #1a1a1a;
