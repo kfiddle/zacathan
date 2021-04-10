b269e6a7e4ce2c6d061a4fe8aa91fbec891159e4
kfiddle <kenjfiddle@gmail.com>
2021-04-10 10:46:28 -0400

    various on dashboard page

--- src/main/resources/static/js/funBackend2.js
+++ src/main/resources/static/js/funBackend2.js
@@ -14,14 +14,60 @@ async function doThingsOnThePage() {
     const seriesData = await fetch("/api/lecture-series-list");
     const fullShebang = await seriesData.json();
 
+    const eraseFormerLectures = () => {
+        while (listToRevise.lastChild) {
+            listToRevise.removeChild(listToRevise.lastChild);
+        }
+    }
+
+    const toggleBackdrop = () => {
+        backdrop.classList.toggle('visible');
+    }
+
+    const toggleModal = () => {
+        testingModal.classList.toggle('visible');
+        toggleBackdrop();
+    }
+
+    const openEditingBox = (lectureTitle) => {
+        toggleModal();
+    }
+
+    const openLectures = seriesTitle => {
+        eraseFormerLectures();
+        fullShebang.forEach(item => {
+            if (item.title === seriesTitle) {
+                item.lectures.forEach(lecture => {
+                    let lectureElem = document.createElement('li');
+                    lectureElem.innerText = lecture.title;
+                    lectureElem.addEventListener('click', ()=> {
+                        openEditingBox(lecture.title);
+                    })
+                    listToRevise.appendChild(lectureElem);
+
+                })
+            }
+        })
+
+
+
+
+
+    }
+
+
     fullShebang.forEach(sheBang => {
         let seriesTitle = document.createElement('li');
         seriesTitle.innerText = sheBang.title;
+        seriesTitle.addEventListener('click', ()=> {
+            openLectures(sheBang.title);
+        });
         menuItemsList.appendChild(seriesTitle);
-        console.log(seriesTitle)
-
-
     })
+
+
+    xButton.addEventListener('click', toggleModal);
+
 }
 
 
